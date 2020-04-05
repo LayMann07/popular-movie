8da6780a4d9a862632db0a189095de225124d7c2
LayMann07 <46538626+LayMann07@users.noreply.github.com>
2020-04-05 11:45:35 +0530

    Initial Commit

---
+++ mov_sug/lib/src/resources/repository.dart
@@ -0,0 +1,9 @@
+import 'dart:async';
+import 'movie_provider.dart';
+import '../models/item_model.dart';
+
+class Repository {
+  final moviesApiProvider = MovieApiProvider();
+
+  Future<ItemModel> fetchAllMovies() => moviesApiProvider.fetchMovieList();
+}
